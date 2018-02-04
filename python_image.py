6c7f7a8c09a72a2a7d4acdc3f2070962f3e85958
cb285 <asapcoffee@protonmail.com>
2018-02-04 12:57:36 -0600

    added padding, binary pbm and pgm reading/writing

--- python_image.py
+++ python_image.py
@@ -4,49 +4,144 @@ import numpy as np
 from copy import deepcopy
 from subprocess import Popen
 from os import setpgrp
+import numbers
 
 class ImageReadError(Exception):
     pass
 
+PADTYPE_NONE = "none"
+PADTYPE_ZERO = "zero"
+PADTYPE_SAME = "same"
+
 class Img():
+
+    """
+    Function: __init__
+    runs when instance of class is created
+    """
     def __init__(self, arg=(0,0)):
         
-        # dimensions
+        # if arg is dimensions, create an empty image of that size
         if(type(arg) is tuple):
             self._size = arg
             self._img_data = np.zeros(self._size, np.uint8)
-            self._dtype = "uint8"
             self._rep = "gray"
-            
-        # filename
+            self._padtype = PADTYPE_NONE
+        
+        # if arg is a filename, read the image
         elif(type(arg) is str):
             self.imread(arg)
+            self._padtype = PADTYPE_NONE
         
-        # image
+        # if arg is another image, copy the image
         elif(type(arg) is type(self)):
             self._size = deepcopy(arg._size) # tuples are immutable, but their elements might not be
             self._img_data = arg._img_data.copy() # arrays are mutable
-            self._dtype = arg._dtype # strings are immutable, so no copy needed
             self._rep = arg._rep
+            self._padtype = PADTYPE_NONE
 
+    """
+    Function: __repr__
+    defines class's print() and repr() behavior
+    """
     def __repr__(self):
+        # print the dimensions
         s = "size:" + str(self._size[0]) + "x" + str(self._size[1]) + "\n"
-        s = s + "dtype:" + self._dtype + "\n"
+        # print the data type
+        s = s + "dtype:" + str(self.dtype()) + "\n"
+        # print the representation
         s = s + "rep:" + self._rep + "\n"
+        # print the array of data
         s = s + str(self._img_data)
         return s
-    
+
+    """
+    Function: __getitem__
+    defines indexing when retrieving elements of image (e.g. a[3, 21])
+    """
     def __getitem__(self, tup):
+        # if index is for three dimensions
         if(len(tup) == 3):
             y, x, z = tup
-            return self._img_data[z, y, x]
+
+            # if this image has three dimensions
+            if(len(self._size) == 3):
+                # check if padding is enabled
+                if(self._padtype == PADTYPE_NONE):
+                    return self._img_data[z, y, x]
+                else:
+                    # check if out of bounds
+                    raise Exception("padding unsupported on 3d images")
+            # if does not have three dimensions, throw exception
+            else:
+                raise ValueError("invalid index for a 2d image")
         
+        # if index is for two dimensions
         if(len(tup) == 2):
             y, x = tup
-            if (len(self._size) == 3):
-                return self._img_data[:, y, x]
+            # if padding is disabled
+            if(self._padtype == PADTYPE_NONE):
+                # if image is three dimensions
+                if (len(self._size) == 3):
+                    # return three values
+                    return self._img_data[:, y, x]
+                # if two dimensions
+                else:
+                    return self._img_data[y, x]
+                # if padding is enabled
             else:
-                return self._img_data[y,x]
+                # if out of bounds
+                if((y < 0) or (y > self._size[0]) or (x < 0) or (x > self._size[1])):
+                    if(self._padtype == PADTYPE_ZERO):
+                        return 0
+                    elif(self._padtype == PADTYPE_SAME):
+                        # if inside x dimensions
+                        if(not ((x < 0) or (x > self._size[1]))):
+                            # if on top of image
+                            if(y < 0):
+                                # return closest pixel
+                                return(self[0, x])
+                            # if on bottom of image
+                            else:
+                                # return closest pixel
+                                return(self[self._size[0] - 1, x])
+                            # if inside y dimensions
+                        elif(not ((y < 0) or (y > self._size[0]))):
+                            # if left of image
+                            if(x < 0):
+                                # return closest pixel
+                                return(self[y, 0])
+                            # if to right of image
+                            else:
+                                # return closest pixel
+                                return(self[y, self._size[1] - 1])
+                            # if outside both x and y dimensions (corners)
+                        else:
+                            # top left
+                            if(x < 0 and y < 0):
+                                # return corner pixel
+                                return(self[0,0])
+                            # top right
+                            elif(y < 0):
+                                # return corner pixel
+                                return(self[0, self._size[1] - 1])
+                            # bottom left
+                            elif(x < 0):
+                                # return corner pixel
+                                return(self[self._size[0] - 1, 0])
+                            # bottom left
+                            else:
+                                # return corner pixel
+                                return(self[self._size[0] - 1, self._size[1] - 1])
+                # if in bounds
+                else:
+                    # if image is three dimensions
+                    if (len(self._size) == 3):
+                        # return three values
+                        return self._img_data[:, y, x]
+                    # if two dimensions
+                    else:
+                        return self._img_data[y, x]
             
     def __setitem__(self, tup, val):
         if(len(tup) == 3):
@@ -60,9 +155,62 @@ class Img():
             else:
                 self._img_data[y,x] = val
 
+    # addition
+    def __add__(self, other):
+        new_img = Img(self)
+        new_img._img_data = new_img._img_data + other
+        return new_img
+    
+    # subtraction
+    def __sub__(self, other):
+        new_img = Img(self)
+        new_img._img_data = new_img._img_data - other
+        return new_img
+    
+    # multiplication (self * other)
+    def __mul__(self, other):
+        new_img = Img(self)
+        new_img._img_data = new_img._img_data * other
+        return new_img
+    
+    # reverse multiplication (other * self)
+    __rmul__ = __mul__
+
+    # division
+    def __truediv__(self, other):
+        new_img = Img(self)
+        new_img._img_data = new_img._img_data // other
+        return new_img
+    
+    # equal
+    def __eq__(self, other):
+        if(type(other) is Img):
+            return np.arrayequal(self._img_data, other._img_data)
+        else:
+            return False
+    
+    def dtype(self, dt=False):
+        if(not dt):
+            return self._img_data.dtype
+        else:
+            self._img_data = self._img_data.astype(dt)
+            return self
+
+    def padtype(self, padtype=False):
+        # if no argument given, return current padtype
+        if(not padtype):
+            return self._padtype
+        
+        if(padtype in [PADTYPE_NONE, PADTYPE_ZERO, PADTYPE_SAME]):
+            self._padtype = padtype
+        else:
+            raise ValueError("invalid argument for padtype")
+        
+        return self
+
     def size(self):
         return self._size
-        
+    
     def imshow(self, title):
         # write to temporary file
         filename = title + ".pbm"
@@ -104,8 +252,7 @@ class Img():
             self._write_pbm(filename)
         # pgm
         elif (ext == "pgm"):
-            raise ImageReadError("invalid/unsupported image format")
-            #self._write_pgm(filename)
+            self._write_pgm(filename)
         # ppm
         elif (ext == "ppm"):
             raise ImageReadError("invalid/unsupported image format")
@@ -114,7 +261,7 @@ class Img():
             raise ImageReadError("invalid/unsupported image format")
 
         return self
-
+    
     def as_type(self, dtype):
         self._img_data = self._img_data.as_type(dtype)
         return self
@@ -126,51 +273,65 @@ class Img():
     def _read_pbm(self, filename):
         
         with open(filename, "rb") as f:
-            
-            l = f.read().split(bytes(b'\n'))
-            print(str(l[0]))
-            #lines = f.readlines()
+            flines = f.read().split(bytes(b'\n'))
         
-        words = list()
+        magic = flines[0].decode("utf-8")
+        n, m = map(int, flines[1].decode("utf-8").split(" "))
+        dlines = flines[2:]
         
-        for line in lines:
-            line_words = line.split(" ")
-            for w in line_words:
-                w.strip()
-                # skip comments
-                if(w[0] == "#"):
-                    break
-                # skip empty words
-                if(w[0] == ""):
-                    continue
-                words.append(w.strip())
-        
-        words = [w for w in words if w]
-                
-        # get "magic number"
-        magic = words[0]
-        # get size
-        size = (int(words[2]), int(words[1])) # (m, n)
-        # get data
-        data = words[3:]
+        # store image info
+        self._size = (m, n)
+        self._img_data = np.zeros(self._size, np.uint8)
+        self._rep = "bw"
         
         # ASCII format
-        if (magic == "P1"):
+        if(magic == "P1"):
             
-            # store image info
-            self._size = size
-            self._img_data = np.zeros(self._size, np.uint8)
-            self._dtype = "uint8"
-            self._rep = "bw"
+            data = list()
+            
+            for line in dlines:
+                line_words = line.decode("utf-8").split(" ")
+                for w in line_words:
+                    # remove special characters
+                    w.strip()
+                    # skip comments and empty words
+                    if(len(w) == 0):
+                        continue
+                    elif(w[0] == "#"):
+                        break
+                    data.append(w.strip())
             
             # put data into array
             for j in range(self._size[0]):
                 for i in range(self._size[1]):
                     self._img_data[j, i] = int(data[self._size[1]*j+i])
-                    
+            
+            return self
+        
         # binary format
         elif (magic == "P4"):
-            pass
+            
+            data = dlines[0]
+            
+            pixel_num = 0
+            byte_num = 0
+            shift = 0
+            
+            # put data into array
+            for j in range(self._size[0]):
+                for i in range(self._size[1]):
+                    pixel_num = self._size[1]*j+i
+                    byte_num = int(pixel_num / 8)
+                    shift = 7 - pixel_num % 8
+                    
+                    #print(pixel_num)
+                    
+                    print((data[byte_num] & (1 << shift)) >> shift)
+                    
+                    self._img_data[j, i] = ((data[byte_num] & (1 << shift)) >> shift)
+                    
+            return self
+            
         else:
             raise ImageReadError("invalid \"magic number\" for pbm")
         
@@ -185,11 +346,80 @@ class Img():
             f.write((str(self._size[1]) + " " + str(self._size[0]) + "\n").encode("utf-8"))
             # write image data
             np.packbits(self._img_data, axis=-1).tofile(f)
-                
+            
+        return self
+
+    def _write_pgm(self, filename):
+        with open(filename, "wb") as f:
+            # write "magic" string
+            f.write("P5\n".encode("utf-8"))
+            # write size
+            f.write((str(self._size[1]) + " " + str(self._size[0]) + "\n").encode("utf-8"))
+            # write max value
+            f.write("255\n".encode("utf-8"))
+            
+            # write image data
+            self._img_data.tofile(f) #unpackbits(self._img_data, , axis=-1)
+            
         return self
     
     def _read_pgm(self, filename):
-        raise ImageReadError("unsupported image format")
+        with open(filename, "rb") as f:
+            flines = f.read().split(bytes(b'\n'))
+        
+        magic = flines[0].decode("utf-8")
+        n, m = map(int, flines[1].decode("utf-8").split(" "))
+        max_val = int(flines[2].decode("utf-8"))
+        dlines = flines[3:]
+        
+        # store image info
+        self._size = (m, n)
+        self._img_data = np.zeros(self._size, np.uint8)
+        self._rep = "bw"
+        
+        # ASCII format
+        if(magic == "P2"):
+            
+            data = list()
+            
+            for line in dlines:
+                line_words = line.decode("utf-8").split(" ")
+                for w in line_words:
+                    # remove special characters
+                    w.strip()
+                    # skip comments and empty words
+                    if(len(w) == 0):
+                        continue
+                    elif(w[0] == "#"):
+                        break
+                    data.append(w.strip())
+            
+            # put data into array
+            for j in range(self._size[0]):
+                for i in range(self._size[1]):
+                    self._img_data[j, i] = int(255*(float(data[self._size[1]*j+i])/max_val))
+            
+            return self
+        
+        # binary format
+        elif (magic == "P5"):
+            
+            data = dlines[0]
+
+            print(len(data))
+            
+            # put data into array
+            for j in range(self._size[0]):
+                for i in range(self._size[1]):
+                    #print(j, i)
+                    self._img_data[j, i] = data[self._size[1]*j+i]
+                    
+            return self
+            
+        else:
+            raise ImageReadError("invalid \"magic number\" for pbm")
+        
+        return self
     
     def _read_ppm(self, filename):
         raise ImageReadError("unsupported image format")
--- test.py
+++ test.py
@@ -1,16 +1,72 @@
 #!/usr/bin/env python3
 
 import sys
-from python_image import Img
+from python_image import *
 
 def main(args):
     
     # create empty image
-    #a = Img("sierpinski.ascii.pbm")
-    a = Img((10,10)).fill(1)
-    a.imwrite("writetest.pbm")
+    a = Img()
 
-    b = Img("writetest.pbm")
+    # read pgm image
+    a.imread('feep.pgm')
+
+    # write binary pgm image
+    a.imwrite("binary_feep.pgm")
+    
+    # create 3x3 image
+    b = Img((3,3))
+    
+    for i in range(3):
+        for j in range(3):
+            b[j,i] = j*3+i
+
+    print(b)
+    
+    # no padding
+    #print(b[4,2])
+    
+    # zero padding
+    print("zero padding:")
+    b.padtype(PADTYPE_ZERO)
+    print("b[6,8] = " + str(b[6,8]))
+    
+    # same padding
+    print("same padding")
+    b.padtype(PADTYPE_SAME)
+    print("b[6,8] = " + str(b[6,8]))
+    print("b[8,1] = " + str(b[8,1]))
+
+    print("\n")
+
+    # read binary pgm
+    a.imread("binary_feep.pgm")
+    
+    # create image 3 times size of a
+    a_size = a.size()
+    mag = 5
+    c = Img((a_size[0]*mag,a_size[1]*mag))
+    
+    c_size = c.size()
+
+    print("a_size = " + str(a_size))
+    print("c_size = " + str(c_size))
+    
+    for j in range(c_size[0]):
+        for i in range(c_size[1]):
+            y = round(j / mag)
+            x = round(i / mag)
+            
+            if(y > a_size[0] - 1):
+                y = a_size[0] - 1
+
+            if(x > a_size[1] - 1):
+                x = a_size[1] - 1
+
+            c[j,i] = a[y, x]
+    
+    # write to file
+    c.imwrite("feep_resized.pgm")
     
 if __name__ == "__main__":
     main(sys.argv)
